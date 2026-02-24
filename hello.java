48ac172a87c9b71bcc44ba2ea99b817ab63f3839
fosslab <testing@gmail.com>
2026-02-24 14:50:41 +0530

    First commit

--- hello.java
+++ hello.java
@@ -4,5 +4,8 @@ public class Hello
     public static void main(String args[])
     {
         System.out.println("Git - VCS");
+	System.out.println("After First Commit");
     }
 }
+rt PATH=$PATH:"C:\Program Files\Java\jdk1.6.0_24\bin"
+
